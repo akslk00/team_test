804796cd7eb510a5eaf02ebed309ff14776095c2
akslk00 <akakquswodud31@gmail.com>
2024-01-22 18:13:00 +0900

    add

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_jwt_extended import JWTManager
 from flask_restful import Api
+from resources.recipes import MyRecipeResource, RecipeResource
 from resources.user import UserDelete, jwt_blocklist
 from config import Config
 from resources.user import UserLoginResource, UserLogoutResource, UserRegisterResource
@@ -25,6 +26,9 @@ api.add_resource( UserLoginResource ,'/user/login')
 api.add_resource( UserLogoutResource,'/user/logout')
 api.add_resource( UserDelete,'/user/delete')
 
+
+api.add_resource(RecipeResource, '/recipes/add')
+api.add_resource(MyRecipeResource,'/myrecipes/<int:Myrecipes_id>')
 
 
 if __name__ == '__main__':
--- resources/recipes.py
+++ resources/recipes.py
@@ -1,46 +1,135 @@
 from flask import request
 from flask_jwt_extended import get_jwt_identity, jwt_required
 from flask_restful import Resource
+from config import Config
 from mysql_connention import get_connection
 from mysql.connector import Error
 
+from datetime import datetime
+
+import boto3
+
+# 레시피 업로드
 class RecipeResource(Resource):
-
+        
     @jwt_required()
-    def post(self):
-   
-        data = request.get_json()
-      
-        user_id=get_jwt_identity()
+    def post(self) :
 
-        print(data)
+        file = request.files.get('photo')
+        title = request.form.get('title')
+        content = request.form.get('content')
+        ingredients = request.form.get('ingredients')
+        recipe = request.form.get('recipe')
+    
+        user_id = get_jwt_identity()
 
-        try:
-           
+        if file is None :
+            return {'error' : '파일을 업로드 하세요'}, 400
+        
+
+        current_time = datetime.now()
+
+        new_file_name = current_time.isoformat().replace(':', '_') + str(user_id) + '.jpg'  
+ 
+        file.filename = new_file_name
+
+        s3 = boto3.client('s3',
+                    aws_access_key_id = Config.AWS_ACCESS_KEY_ID,
+                    aws_secret_access_key = Config.AWS_SECRET_ACCESS_KEY )
+
+        try :
+            s3.upload_fileobj(file, 
+                              Config.S3_BUCKET,
+                              file.filename,
+                              ExtraArgs = {'ACL' : 'public-read' , 
+                                           'ContentType' : 'image/jpeg'} )
+        except Exception as e :
+            print(e)
+            return {'error' : str(e)}, 500
+        
+        try :
             connection = get_connection()
 
-            query ='''insert into posting
+            query = '''insert into posting
                         (userId, title,content, imageURL, ingredients,recipe)
                         values
                         (%s,%s,%s,%s,%s,%s);'''
-           
-            record = (data['userId'],data['title'],data['content'],
-                      data['imageURL'],data['ingredients'],user_id)
-            
+            record = (user_id,title,content,
+                      Config.S3_LOCATION+new_file_name,ingredients,recipe,
+                      )
             cursor = connection.cursor()
-
-            cursor.execute(query,record)
-
+            cursor.execute(query, record)
             connection.commit()
 
             cursor.close()
             connection.close()
 
-
         except Error as e:
             print(e)
             cursor.close()
             connection.close()
-            return {"result":"fail","error":str(e)}, 500
-        
-        return {"result":"success"}, 200
+            return {'error' : str(e)}, 500
+
+
+        return {'result' : 'success'}, 200
+    
+# 레시피 수정, 삭제
+class MyRecipeResource(Resource):
+
+    # 수정
+    @jwt_required()
+    def put(self,Myrecipes_id):
+
+        file = request.files.get('photo')
+        title = request.form.get('title')
+        content = request.form.get('content')
+        ingredients = request.form.get('ingredients')
+        recipe = request.form.get('recipe')
+
+        user_id=get_jwt_identity()
+
+        current_time = datetime.now()
+
+        new_file_name = current_time.isoformat().replace(':', '_') + str(user_id) + '.jpg'  
+ 
+        file.filename = new_file_name
+
+        s3 = boto3.client('s3',
+                    aws_access_key_id = Config.AWS_ACCESS_KEY_ID,
+                    aws_secret_access_key = Config.AWS_SECRET_ACCESS_KEY )
+
+        try :
+            s3.upload_fileobj(file, 
+                              Config.S3_BUCKET,
+                              file.filename,
+                              ExtraArgs = {'ACL' : 'public-read' , 
+                                           'ContentType' : 'image/jpeg'} )
+        except Exception as e :
+            print(e)
+            return {'error' : str(e)}, 500
+
+        try:
+            connection = get_connection()
+            query = '''update Memo
+                        set title=%s,
+                            content = %s,
+                            imageURL = %s,
+                            ingredients = %s,
+                            recipe = %s
+                        where id=%s and userId=%s;'''
+            record =(title,content,Config.S3_LOCATION+new_file_name,ingredients,recipe,Myrecipes_id,user_id)
+
+            cursor = connection.cursor()
+            cursor.execute(query,record)
+            connection.commit()
+
+            cursor.close()
+            connection.close()
+        except Error as e:
+            print(e)
+            cursor.close()
+            connection.close()
+            return{'error':str(e)},500
+            
+        return{'result':'success'},200
+
