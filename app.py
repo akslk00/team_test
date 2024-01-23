270fb0df6493fa942ee3a4789dc2c076cbd4a009
YoonGeonSON <151480776+YoonGeonSON@users.noreply.github.com>
2024-01-23 11:51:52 +0900

    modified:   app.py 	modified:   resources/user.py

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
 from flask import Flask
 from flask_jwt_extended import JWTManager
 from flask_restful import Api
-<<<<<<< HEAD
 from resources.recipe import RecipeDetail, RecipeFollow, RecipeListResource, RecipeMeResource, RecipeResource
-=======
-from resources.recipes import MyRecipeResource, RecipeResource
->>>>>>> 742c2b127ee2b11fc1d2ba492b361a682b3ef4d0
-from resources.user import UserDelete, jwt_blocklist
+from resources.user import UserDelete, UserPasswordUpdate, jwt_blocklist
 from config import Config
 from resources.user import UserLoginResource, UserLogoutResource, UserRegisterResource
 
@@ -29,18 +25,14 @@ api.add_resource( UserRegisterResource, '/user/register')
 api.add_resource( UserLoginResource ,'/user/login')
 api.add_resource( UserLogoutResource,'/user/logout')
 api.add_resource( UserDelete,'/user/delete')
+api.add_resource( UserPasswordUpdate,'/user/passwordUpdate')
 
 
-<<<<<<< HEAD
 api.add_resource(RecipeListResource, '/recipe')
 api.add_resource( RecipeDetail , '/recipe/<int:posting_id>')
 api.add_resource(RecipeMeResource, '/myrecipe')
 api.add_resource(RecipeFollow, '/followrecipe')
 
-=======
-api.add_resource(RecipeResource, '/recipes/add')
-api.add_resource(MyRecipeResource,'/myrecipes/<int:Myrecipes_id>')
->>>>>>> 742c2b127ee2b11fc1d2ba492b361a682b3ef4d0
 
 
 if __name__ == '__main__':
