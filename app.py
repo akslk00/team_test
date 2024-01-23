b8d50b0d925e2c70609256f53158d964072d2283
isk03300 <isk03300@naver.com>
2024-01-23 09:35:30 +0900

    add

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_jwt_extended import JWTManager
 from flask_restful import Api
+from resources.recipe import RecipeDetail, RecipeFollow, RecipeListResource, RecipeMeResource, RecipeResource
 from resources.user import UserDelete, jwt_blocklist
 from config import Config
 from resources.user import UserLoginResource, UserLogoutResource, UserRegisterResource
@@ -24,6 +25,12 @@ api.add_resource( UserRegisterResource, '/user/register')
 api.add_resource( UserLoginResource ,'/user/login')
 api.add_resource( UserLogoutResource,'/user/logout')
 api.add_resource( UserDelete,'/user/delete')
+
+
+api.add_resource(RecipeListResource, '/recipe')
+api.add_resource( RecipeDetail , '/recipe/<int:posting_id>')
+api.add_resource(RecipeMeResource, '/myrecipe')
+api.add_resource(RecipeFollow, '/followrecipe')
 
 
 
