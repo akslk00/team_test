f4e282cf3a08c71dced4e2990d9db038f782c9d7
isk03300 <isk03300@naver.com>
2024-01-23 16:46:45 +0900

    add

--- resources/review.py
+++ resources/review.py
@@ -15,8 +15,8 @@ class ReviewResource(Resource):
         data = request.get_json()
         user_id = get_jwt_identity()
 
-        if int(data['rating'])<1 and int(data['rating'])>6:
-            return{'error': '별점은 1부터 5까지만 가능 합니다.'},400
+        if int(data['rating']) < 1 or int(data['rating']) > 5:
+            return {'error': '별점은 1부터 5까지만 가능합니다.'}, 400
         
         try :
             connection = get_connection()
